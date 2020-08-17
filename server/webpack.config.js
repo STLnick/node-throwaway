b8478c33dbf0e3c3274448cf3911ad9bfad9e93f
Nick Ray <nickrray@gmail.com>
2020-08-17 13:09:30 -0500

    :rotating_light: Change double to single quotes

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  target: "node",
-  mode: "development",
-  entry: "./server.js",
+  target: 'node',
+  mode: 'development',
+  entry: './server.js',
   resolve: {
-    modules: ["backend", "node_modules"],
+    modules: ['backend', 'node_modules'],
   },
-}
+};
