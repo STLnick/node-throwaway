131f13880bbaa9e02f6c494fe8b983822fbcd8ae
Nick Ray <nickrray@gmail.com>
2020-08-19 23:20:50 -0500

    :art: Rename fxn and parameters for clarity

--- client/scripts.js
+++ client/scripts.js
@@ -1,10 +1,10 @@
-const postDeets = async (deets) => {
+const addUser = async (newUser) => {
   const res = await fetch('http://localhost:5000/users/add', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(deets)
+    body: JSON.stringify(newUser)
   })
 }
 
@@ -21,14 +21,13 @@ const deleteUser = async (phoneNum) => {
 document.querySelector(".add-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  postDeets(Array.from(event.target.elements)
+  addUser(Array.from(event.target.elements)
     .filter(({ dataset }) => dataset.form)
-    .reduce((submissionDeets, currentEl) => {
-      submissionDeets[currentEl.id] = currentEl.value;
-      return submissionDeets;
-    }, {}));
-
-});
+    .reduce((userInfo, input) => {
+      userInfo[input.id] = input.value;
+      return userInfo;
+    }, {}))
+})
 
 document.querySelector('.delete-form').addEventListener('submit', (e) => {
   e.preventDefault()
